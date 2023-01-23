89aeb73b50c4651bf67ea3bd022077c909f209f6
slendyG <josuegarza0608@gmail.com>
2023-01-23 01:12:29 -0800

    set up state for dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      
+      <Form />
     </div>
   );
 }
