89278fee295cbf9328275e6655b795cc6b292098
slendyG <josuegarza0608@gmail.com>
2023-01-24 01:11:16 -0800

    changes to styling of the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className='container'>
+      <h1>Login Form</h1>
+      <form>
+        <div className='form-group'>
+          <label htmlFor="email" className='form-label'>Enter Email Here!</label>
+          <input type="text" className='form-control' placeholder='test@email.com'/>
+        </div>
+        <div className='form-group'>
+          <label htmlFor="password" className='form-label'>Enter Password Here!</label>
+          <input type="text" className='form-control' placeholder='password'/>
+        </div>
+        <button className="btn btn-primary" type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
