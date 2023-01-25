74ece1fec368ca2e5e288cfba512589d2b60518c
slendyG <josuegarza0608@gmail.com>
2023-01-25 01:51:39 -0800

    changes to the form styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { FaTwitterSquare, FaGoogle, FaLinkedin, FaFacebookSquare } from 'react-icons/fa';
+
 
 const Form = () => {
   return (
-    <div className='container'>
+    <div className='main'>
       <h1>Login Form</h1>
-      <form>
+      <form className='container'>
+        <div className="top">
+          <FaFacebookSquare />
+          <FaGoogle />
+          <FaLinkedin />
+          <FaTwitterSquare />
+        </div>
         <div className='form-group'>
           <label htmlFor="email" className='form-label'>Enter Email Here!</label>
           <input type="text" className='form-control' placeholder='test@email.com'/>
@@ -13,10 +21,10 @@ const Form = () => {
           <label htmlFor="password" className='form-label'>Enter Password Here!</label>
           <input type="text" className='form-control' placeholder='password'/>
         </div>
-        <button className="btn btn-primary" type="submit">Submit</button>
+        <button className="btn btn-primary m-4 " type="submit">Submit</button>
       </form>
     </div>
   )
-}
+};
 
 export default Form
