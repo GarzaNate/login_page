57b946934caade472edb8a89e563886ecbbcf2f6
slendyG <josuegarza0608@gmail.com>
2023-01-29 21:09:47 -0800

    more styling changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import { FaTwitterSquare, FaGoogle, FaLinkedin, FaFacebookSquare } from 'react-icons/fa';
-
+import React from "react";
+import {
+  FaTwitterSquare,
+  FaGoogle,
+  FaLinkedin,
+  FaFacebookSquare,
+} from "react-icons/fa";
 
 const Form = () => {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>Login Form</h1>
-      <form className='container'>
-        <div className="top">
-          <FaFacebookSquare />
-          <FaGoogle />
-          <FaLinkedin />
-          <FaTwitterSquare />
+      <div className="top">
+        <FaGoogle />
+        <FaFacebookSquare />
+        <FaLinkedin />
+        <FaTwitterSquare />
+      </div>
+      <p className="divider"><span>Or</span></p>
+      <form className="container">
+        <div className="form-group">
+          <label htmlFor="email" className="form-label">
+            Enter Email Here!
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="test@email.com"
+          />
         </div>
-        <div className='form-group'>
-          <label htmlFor="email" className='form-label'>Enter Email Here!</label>
-          <input type="text" className='form-control' placeholder='test@email.com'/>
+        <div className="form-group">
+          <label htmlFor="password" className="form-label">
+            Enter Password Here!
+          </label>
+          <input type="text" className="form-control" placeholder="password" />
         </div>
-        <div className='form-group'>
-          <label htmlFor="password" className='form-label'>Enter Password Here!</label>
-          <input type="text" className='form-control' placeholder='password'/>
-        </div>
-        <button className="btn btn-primary m-4 " type="submit">Submit</button>
+        <button className="btn btn-primary m-4" type="submit">
+          Login
+        </button>
       </form>
     </div>
-  )
+  );
 };
 
-export default Form
+export default Form;
